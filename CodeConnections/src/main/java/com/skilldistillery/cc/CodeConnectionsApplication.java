58cd2e98f7a53c0d6d255b9da6523c31a9c7b15d
Christina <christina.pitch@gmail.com>
2020-11-13 16:41:57 -0700

    stub boot project with jpa dependencies

---
+++ CodeConnections/src/main/java/com/skilldistillery/cc/CodeConnectionsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.cc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeConnectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeConnectionsApplication.class, args);
+	}
+
+}
